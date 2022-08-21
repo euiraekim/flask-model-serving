5eddb01c808bf05044b0f5cc6bcc999b81c80409
euiraekim <harrykur139@gmail.com>
2022-08-21 15:47:17 +0900

    test code, jenkinsfile, docker test file 추가

---
+++ test.py
@@ -0,0 +1,35 @@
+import json
+
+import unittest
+from app import app
+
+CLASSES = ['setosa', 'versicolor', 'virginica']
+
+class TestFlask(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_health(self):
+        rv = self.app.get('/health')
+        self.assertEqual(rv.status, '200 OK')
+
+    def test_predict(self):
+        params = {
+            "sepal_length": 6.5,
+            "sepal_width": 3.0,
+            "petal_length": 4.7,
+            "petal_width": 2.5
+        }
+        rv = self.app.get(
+            "/predict",
+            data=json.dumps(params),
+            content_type="application/json"
+        )
+
+        data = json.loads(rv.data)
+        self.assertEqual(rv.status, '200 OK')
+        self.assertIn(data['class'], CLASSES)
+        
+if __name__ == '__main__':
+    unittest.main()
